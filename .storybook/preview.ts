872f4c783e8695210fa5b30697f5ba1afe26e754
JayChevli18 <chevlijay70@gmail.com>
2025-02-19 16:49:22 +0530

    Changes in Config for Dark Mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,17 @@
 import type { Preview } from "@storybook/react";
 import "../src/app/globals.css";
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
+    docs:{
+      theme: themes.light
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+
       },
     },
   },
